daa43d7a6e03025b0579f3bc888e42841b74a26a
Berg Paulo <lindembergdepaulo@gmail.com>
2019-02-02 11:24:16 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace 'api' do
-  	namespace 'v1' do
-  		resources :articles
-  	end
-  end
+
+  get '/', to: 'articles#index'
+  get '/show/:id', to: 'articles#show'
+  post '/create', to: 'articles#create'
+  patch '/update/:id', to: 'articles#update'
+  delete '/delete/:id', to: 'articles#destroy'
 end
